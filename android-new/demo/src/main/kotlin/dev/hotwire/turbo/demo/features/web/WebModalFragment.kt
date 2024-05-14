a2cd0c9a62fd9f37a2597ba1ce8db434042598d5
Matthew Blott <m4ttb@pm.me>
2024-05-14 14:27:29 +0100

    saved

---
+++ android-new/demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebModalFragment.kt
@@ -0,0 +1,6 @@
+package dev.hotwire.turbo.demo.features.web
+
+import dev.hotwire.turbo.nav.TurboNavGraphDestination
+
+@TurboNavGraphDestination(uri = "turbo://fragment/web/modal")
+class WebModalFragment : WebFragment()
