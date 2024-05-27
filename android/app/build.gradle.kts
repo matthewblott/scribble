17a173ffea1dc2cc3dd1112d508b884d1ab0dba8
Matthew Blott <m4ttb@pm.me>
2024-05-27 23:53:59 +0100

    Tidied up the Android code, really stripped it down to the smallest possible size and use with minimal libraries and still got everything to build.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+  id("com.android.application") version "8.4.1"
+  id("org.jetbrains.kotlin.android") version "1.9.0"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+}
+
+android {
+  namespace = "com.example.scribble"
+  compileSdk = 34
+  defaultConfig {
+    minSdk = 34
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+}
+
+dependencies {
+  implementation("androidx.activity:activity-compose:1.9.0")
+  implementation("androidx.core:core-ktx:1.13.1")
+
+  // implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation("androidx.recyclerview:recyclerview:1.3.2")
+  implementation("androidx.browser:browser:1.8.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+  implementation("com.github.bumptech.glide:glide:4.16.0")
+  implementation("dev.hotwire:strada:1.0.0-beta3")
+  implementation("dev.hotwire:turbo:7.1.3")
+
+  // classpath("com.android.tools.build:gradle:8.4.1")
+  // classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+}
